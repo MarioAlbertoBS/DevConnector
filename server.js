27817162baf38f5df7ca5c5b6e8b023e1639d063
MarioAlbertoBS <maralbo1@hotmail.com>
2020-05-25 10:58:31 -0700

    Added main resource routes

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ connectDb();
 //Home route
 app.get("/", (req, res) => res.send('API running'));
 
+//Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 //Define port from env file or local if doesn't exists
 const PORT = process.env.PORT || 5000;
 
