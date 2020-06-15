99412311136ee18e4b3f4b01b034314987db6dda
MarioAlbertoBS <maralbo1@hotmail.com>
2020-06-15 11:12:06 -0700

    Created Dashboard Private Route

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Dashboard = (props) => {
+    return (
+        <section className="container">
+            DASHBOARD
+        </section>
+    )
+}
+
+Dashboard.propTypes = {
+
+}
+
+export default Dashboard
+
