a21413c9abda4281fbae4c08e931d933f145ef61
MarioAlbertoBS <maralbo1@hotmail.com>
2020-06-16 11:31:03 -0700

    Added delete posts at delete account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 //@route   GET api/profile/me
 //@desc    Get the current users profile
@@ -141,7 +142,9 @@ router.get('/user/:user_id', async (req, res) => {
 //access   Private
 router.delete('/', auth, async (req, res) => {
     try {
-        //@todo: remove users posts
+        //remove users posts
+        await Post.deleteMany({user: req.user.id });
+
         //Remove profile
         await Profile.findOneAndRemove({ user: req.user });
         
