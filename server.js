c5e9313940eab717d6fe343e703b45f6e0125578
MarioAlbertoBS <maralbo1@hotmail.com>
2020-06-20 14:20:45 -0700

    Prepared App to be build in Heroku

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //Set express server
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const connectDb = require('./config/db');
+const path = require("path");
+
+const connectDb = require("./config/db");
 
 //Connect db
 connectDb();
@@ -10,14 +12,21 @@ connectDb();
 //Init middleware
 app.use(express.json({ extended: false }));
 
-//Home route
-app.get("/", (req, res) => res.send('API running'));
-
 //Define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    //Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 //Define port from env file or local if doesn't exists
 const PORT = process.env.PORT || 5000;
